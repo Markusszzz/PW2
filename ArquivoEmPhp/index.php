6a8a026c529bc85e800fd149c9e25542c0fc7eef
Marcus Vinícius dos Santos <112128360+Markusszzz@users.noreply.github.com>
2023-03-03 22:02:10 -0300

    Add files via upload

---
+++ ArquivoEmPhp/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PrimeiroPhpDoNego.com</title>
+    </head>
+    <body>
+        
+            <h1>Olá Mundo</h1>
+        
+            <?php
+            
+                $nome = "Marcus";
+                $idade = 16;
+                $peso = 80.00;
+
+                $num1 = 24;
+                $num2 = 4;
+
+                $soma = $num1 + $num2;
+                $sub = $num1 - $num2;
+                $mult = $num1 * $num2;
+                $div = $num1 / $num2;
+                $rest = $num1 % $num2;
+
+                echo "555" . " + " . "71" . "= " . $soma . "<br />";
+
+                echo "$num1 - $num2 = $sub <br />";
+
+                echo "$num1 * $num2 = $mult <br />";
+
+                echo "$num1 / $num2 = $div <br />";
+
+                echo "$num1 & $num2 = $rest <br />";
+
+
+                
+                echo $nome . "<br />";
+
+                echo "Bem-vindo " . $nome . "<br />";
+
+                echo "seu peso é: " . $peso . "<br />";
+
+                echo "sua idade é: " . $idade;
+
+                
+
+
+
+               
+                
+            
+            
+            ?>
+
+
+        
+
+    </body>
+</html>
